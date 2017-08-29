0d234d47d1b250724eb02bd040c72526e1e4d949
Thomas Hauth <Thomas.Hauth@web.de>
2017-08-29 14:14:47 +0200

    style fix

--- src/SpheresEngine/common/SpheresEngine/RenderEngine/CommonOpenGL/MeshBackend.cpp
+++ src/SpheresEngine/common/SpheresEngine/RenderEngine/CommonOpenGL/MeshBackend.cpp
@@ -48,17 +48,17 @@ Mesh MeshBackend::loadMesh(std::string name, Texture &,
 
 	// pass on vertex position
 	glEnableVertexAttribArray(shaderProg.getAttribLocation("vert"));
-	glVertexAttribPointer(shaderProg.getAttribLocation("vert"), // vertex pos
-	3, // will be three (x,y,z) numbers
-			GL_FLOAT, // of float
-			GL_FALSE, // not normalized
-			combinedBufferSize, // stride (in bytes) is the # of elemnts
+	glVertexAttribPointer(shaderProg.getAttribLocation("vert"),	// vertex pos
+	3,	// will be three (x,y,z) numbers
+			GL_FLOAT,	// of float
+			GL_FALSE,	// not normalized
+			combinedBufferSize,	// stride (in bytes) is the # of elemnts
 			NULL);
 
 	// pass on vertex texture coordinates
 	glEnableVertexAttribArray(shaderProg.getAttribLocation("vertTexCoord"));
-	glVertexAttribPointer(shaderProg.getAttribLocation("vertTexCoord"), // uv coords
-	2, // will have two entries
+	glVertexAttribPointer(shaderProg.getAttribLocation("vertTexCoord"),	// uv coords
+	2,	// will have two entries
 			GL_FLOAT, GL_TRUE, combinedBufferSize,
 			/* The offset within the buffer is 3, because the xyz coords come first*/
 			(const GLvoid*) (3 * sizeof(GLfloat)));
