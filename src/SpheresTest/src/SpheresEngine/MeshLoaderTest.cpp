1a16ce9865c4e8f3ab46935037213d47b315692d
Thomas Hauth <Thomas.Hauth@web.de>
2017-08-29 14:13:31 +0200

    extended test to cover loading two meshes from one file

--- src/SpheresTest/src/SpheresEngine/MeshLoaderTest.cpp
+++ src/SpheresTest/src/SpheresEngine/MeshLoaderTest.cpp
@@ -55,10 +55,13 @@ TEST(MeshLoaderTest, prepare) {
 
 	MeshLoader ld;
 
+	// this mesh data contains two objects, make user we
+	// can entangle them
+	// Important the vertices defined are shared between all obects
 	std::string meshData = "# Blender v2.71 (sub 0) OBJ File: ''\n"
 			"# www.blender.org\n"
 			"mtllib untitled.mtl\n"
-			"o Cube\n"
+			"o FirstCube\n"
 			"v 0.967184 -1.000000 -1.000000\n"
 			"v 0.967184 -1.000000 1.000000\n"
 			"v -1.032816 -1.000000 1.000000\n"
@@ -86,27 +89,66 @@ TEST(MeshLoaderTest, prepare) {
 			"f 2/1/3 1/1/3 6/1/3\n"
 			"f 3/1/4 2/1/4 7/1/4\n"
 			"f 4/1/5 3/1/5 8/1/5\n"
-			"f 5/1/6 1/1/6 8/1/6\n";
+			"f 5/1/6 1/1/6 8/1/6\n"
+			"o SecondCube\n"
+			"v 0.867184 -1.000000 -3.000000\n"
+			"v 0.967184 -1.000000 1.000000\n"
+			"v -1.032816 -1.000000 1.000000\n"
+			"v -1.032816 -1.000000 -1.000000\n"
+			"v 0.967184 1.000000 -0.999999\n"
+			"v 0.967183 1.000000 1.000001\n"
+			"v -1.032816 1.000000 1.000000\n"
+			"v -1.032816 1.000000 -1.000000\n"
+			"vt 1.0 1.0\n"
+			"vn 0.000000 -1.000000 0.000000\n"
+			"vn 0.000000 1.000000 0.000000\n"
+			"vn 1.000000 -0.000000 0.000000\n"
+			"vn -0.000000 -0.000000 1.000000\n"
+			"vn -1.000000 -0.000000 -0.000000\n"
+			"usemtl Material\n"
+			"s off\n"
+			"f 9/1/1 3/1/1 4/1/1\n"
+			"f 8/1/2 7/1/2 6/1/2\n";
 
-	auto md = ld.loadMesh(meshData);
+	// only one object in the mesh data, take the first
+	auto mdMap = ld.loadMesh(meshData);
+	ASSERT_EQ(size_t(2), mdMap.size());
+	auto firstMesh = mdMap["FirstCube"];
+	auto secondMesh = mdMap["SecondCube"];
 
 	const size_t faceCount = 12;
 
-	ASSERT_EQ(size_t(12 * 3), md.Position.size());
-	// no UV coordinates, as they are not in the input file
-	ASSERT_EQ(size_t(36), md.UV.size());
+	ASSERT_EQ(size_t(faceCount * 3), firstMesh.Position.size());
+	ASSERT_EQ(size_t(36), firstMesh.UV.size());
 
-	ASSERT_FLOAT_EQ(0.967184, md.Position[0].x());
-	ASSERT_FLOAT_EQ(1.000000, md.Position[0].z());
+	ASSERT_FLOAT_EQ(0.967184, firstMesh.Position[0].x());
+	ASSERT_FLOAT_EQ(1.000000, firstMesh.Position[0].z());
 
-	ASSERT_FLOAT_EQ(-1.000000, md.Normal[0].y());
+	ASSERT_FLOAT_EQ(-1.000000, firstMesh.Normal[0].y());
 
-	auto flatMesh = md.asXYZUVNormals();
+	auto flatMesh = firstMesh.asXYZUVNormals();
 	ASSERT_FLOAT_EQ(0.967184, flatMesh[0]);
 	ASSERT_FLOAT_EQ(1.000000, flatMesh[2]);
 
 	ASSERT_FLOAT_EQ(0.0, flatMesh[5]);
 	ASSERT_FLOAT_EQ(-1.000000, flatMesh[6]);
+
+	const size_t faceCountSecond = 2;
+	// check the second mesh was also loaded
+	ASSERT_EQ(size_t(faceCountSecond * 3), secondMesh.Position.size());
+	ASSERT_EQ(size_t(faceCountSecond * 3), secondMesh.UV.size());
+
+	ASSERT_FLOAT_EQ(0.867184, secondMesh.Position[0].x());
+	ASSERT_FLOAT_EQ(-3.000000, secondMesh.Position[0].z());
+	ASSERT_FLOAT_EQ(-1.000000, secondMesh.Normal[0].y());
+
+	flatMesh = firstMesh.asXYZUVNormals();
+	ASSERT_FLOAT_EQ(0.967184, flatMesh[0]);
+	ASSERT_FLOAT_EQ(1.000000, flatMesh[2]);
+
+	ASSERT_FLOAT_EQ(0.0, flatMesh[5]);
+	ASSERT_FLOAT_EQ(-1.000000, flatMesh[6]);
+
 }
 
 TEST(MeshLoaderTest, prepareNoUvInfo) {
@@ -145,7 +187,7 @@ TEST(MeshLoaderTest, prepareNoUvInfo) {
 			"f 4//5 3//5 8//5\n"
 			"f 5//6 1//6 8//6\n";
 
-	auto md = ld.loadMesh(meshData);
+	auto md = ld.loadMesh(meshData).begin()->second;
 
 	const size_t faceCount = 12;
 
