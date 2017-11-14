e0aa4facddf7d3a93da75c9aea6db796cb1badd0
Thomas Hauth <Thomas.Hauth@web.de>
2017-11-14 21:15:11 +0100

    added first code to create collision objects

--- src/SpheresEngine/common/SpheresEngine/PhysicsEngine/PhysicsEngine.cpp
+++ src/SpheresEngine/common/SpheresEngine/PhysicsEngine/PhysicsEngine.cpp
@@ -1,10 +1,51 @@
 #include <SpheresEngine/PhysicsEngine/PhysicsEngine.h>
 #include <SpheresEngine/EntityEngine/CommonEntities/CollisionMixin.h>
 
+PhysicsEngine::PhysicsEngine() {
+	m_collisionConfig = std14::make_unique<btDefaultCollisionConfiguration>();
+	m_dispatcher = std14::make_unique<btCollisionDispatcher>(
+			m_collisionConfig.get());
+	m_overlapPair = std14::make_unique<btDbvtBroadphase>();
+	m_solver = std14::make_unique<btSequentialImpulseConstraintSolver>();
+	m_world = std14::make_unique<btDiscreteDynamicsWorld>(m_dispatcher.get(),
+			m_overlapPair.get(), m_solver.get(), m_collisionConfig.get());
+}
+
 void PhysicsEngine::addEntity(Entity * et) {
 	// ensure this entity supports either physics or collisions
-	CollisionMixin * cmixin = dynamic_cast<CollisionMixin*> (et);
+	CollisionMixin * cmixin = dynamic_cast<CollisionMixin*>(et);
 	if (!cmixin) {
-		logging::Fatal() << "Entity added to physics engine does not support collisions.";
+		logging::Fatal()
+				<< "Entity added to physics engine does not support collisions.";
 	}
+
+	if (cmixin->getShapeSource() == CollisionMixin::ShapeSource::Box) {
+		btCollisionShape* shape = new btBoxShape(
+				BulletUtil::toBullet(cmixin->getBoxSize()));
+		// todo: make sure the collision shape is stored and cleaned up later
+		//collisionShapes.push_back(groundShape);
+
+		btTransform groundTransform;
+		groundTransform.setIdentity();
+		groundTransform.setOrigin(btVector3(0, 0, 0));
+
+		// not dynamic with mass 0
+		btScalar mass(0.);
+		btVector3 localInertia(0, 0, 0);
+
+		//using motionstate is optional, it provides interpolation capabilities, and only synchronizes 'active' objects
+		btDefaultMotionState* myMotionState = new btDefaultMotionState(
+				groundTransform);
+		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState,
+				shape, localInertia);
+		btRigidBody* body = new btRigidBody(rbInfo);
+
+		//add the body to the dynamics world
+		m_world->addRigidBody(body);
+
+		logging::Info() << "PhysicsEntity of shape box registered";
+	} else {
+		logging::Fatal() << "Unsupported collision shape";
+	}
+
 }
--- src/SpheresEngine/common/SpheresEngine/PhysicsEngine/PhysicsEngine.h
+++ src/SpheresEngine/common/SpheresEngine/PhysicsEngine/PhysicsEngine.h
@@ -2,11 +2,35 @@
 
 #include <SpheresEngine/EntityEngine/Entity.h>
 
+#include <btBulletDynamicsCommon.h>
+
+#include <memory>
+
+class BulletUtil {
+public:
+
+	static btVector3 toBullet(Vector3 v) {
+		return btVector3(v.x(), v.y(), v.z());
+	}
+
+	static Vector3 toSpheres(btVector3 v) {
+		return Vector3(v.x(), v.y(), v.z());
+	}
+};
+/*
+struct {
+	Entity * Ent;
+
+};
+*/
 /**
  * Place holder for the physics engine which will be added later
  */
 class PhysicsEngine {
 public:
+
+	explicit PhysicsEngine();
+
 	/**
 	 * not implemented atm.
 	 */
@@ -16,5 +40,15 @@ public:
 	/*
 	 * Setup and include an entity for the physics simulation step
 	 */
-	void addEntity( Entity * et );
+	void addEntity(Entity * et);
+
+private:
+
+	// the order of the following members is important so the bullet objects
+	// are destroyed reverse order of dependency
+	std::unique_ptr<btDiscreteDynamicsWorld> m_world;
+	std::unique_ptr<btSequentialImpulseConstraintSolver> m_solver;
+	std::unique_ptr<btDbvtBroadphase> m_overlapPair;
+	std::unique_ptr<btCollisionDispatcher> m_dispatcher;
+	std::unique_ptr<btDefaultCollisionConfiguration> m_collisionConfig;
 };
