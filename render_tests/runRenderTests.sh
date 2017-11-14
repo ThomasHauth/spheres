a3373bdd93d49edaaa66113dae02e628d3e6f16d
Thomas Hauth <Thomas.Hauth@web.de>
2017-11-14 23:23:07 +0100

    added render tests

---
+++ render_tests/runRenderTests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd build/SpheresTestBench/
+# just to be sure ...
+make -j4
+# make sure the old ones are deleted
+rm benchmark_out.bmp
+./SpheresTestBench --automate
+compare ../../render_tests/CubeBenchmark.bmp  benchmark_out.bmp  -compose src out.bmp
+if [ $? -ne 0 ]; then
+    echo "CubeBenchmark is not equal to reference"
+    exit 1
+fi
+echo "All Render tests successful !!"
+
